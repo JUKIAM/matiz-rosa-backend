80f3da9b67a934ad84191e00d5dd3e07c77fd934
JUKIAM <juliandgarcia@unicesar.edu.co>
2023-11-24 13:10:49 -0500

    configuración inicial del proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,34 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { CategoriasModule } from './categorias/categorias.module';
 import { ProductosModule } from './productos/productos.module';
 import { UsuariosModule } from './usuarios/usuarios.module';
 import { VentasModule } from './ventas/ventas.module';
 import { DetalleVentasModule } from './detalle-ventas/detalle-ventas.module';
 import { CarritosModule } from './carritos/carritos.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CategoriasModule, ProductosModule, UsuariosModule, VentasModule, DetalleVentasModule, CarritosModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    CategoriasModule,
+    ProductosModule,
+    UsuariosModule,
+    VentasModule,
+    DetalleVentasModule,
+    CarritosModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  const swaggerConfig = new DocumentBuilder()
+    .addBearerAuth()
+    .setTitle('Travely API')
+    .setDescription('API RESTful para la gestión de viajes')
+    .setVersion('1.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, swaggerDocument);
+
+  const PORT = process.env.PORT || 8000;
+
+  app.enableCors();
+
+  await app.listen(PORT);
+
+  const logger = new Logger('Bootstrap');
+  logger.log(`App is running on port: ${PORT}`);
 }
 bootstrap();
