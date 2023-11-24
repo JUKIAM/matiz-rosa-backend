4bb9614657c3f9f527d1d940522a7a1e78e55909
JUKIAM <juliandgarcia@unicesar.edu.co>
2023-11-24 13:18:29 -0500

    agregando los campos de categoría de productos

--- src/categorias/dto/create-categoria.dto.ts
+++ src/categorias/dto/create-categoria.dto.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateCategoriaDto {
-    nombre: string;
+  @ApiProperty({
+    description: 'Nombre de la categoría del producto.',
+    example: 'Alcancía',
+  })
+  @IsString()
+  @IsNotEmpty()
+  nombre: string;
 }
--- src/categorias/entities/categoria.entity.ts
+++ src/categorias/entities/categoria.entity.ts
@@ -1,4 +1,19 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Categoria {
-    id: number;
-    nombre: string;
+  @PrimaryGeneratedColumn('increment')
+  @ApiProperty({
+    description: 'Id de la categoría del producto.',
+    example: '1',
+  })
+  id: number;
+
+  @Column('varchar', { length: 50, unique: true })
+  @ApiProperty({
+    description: 'Nombre de la categoría del producto.',
+    example: 'Alcancía',
+  })
+  nombre: string;
 }
