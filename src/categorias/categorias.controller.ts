81635bde55a089c225768addb37bf613a30574f0
JUKIAM <juliandgarcia@unicesar.edu.co>
2023-11-24 15:00:49 -0500

    agregamos CRUD de categorías de productos

--- src/categorias/categorias.controller.ts
+++ src/categorias/categorias.controller.ts
@@ -1,33 +1,83 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { CategoriasService } from './categorias.service';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import {
+  ApiBearerAuth,
+  ApiOperation,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Categoria } from './entities/categoria.entity';
 
+//@ApiBearerAuth()
+@ApiTags('categorias')
 @Controller('categorias')
 export class CategoriasController {
   constructor(private readonly categoriasService: CategoriasService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear una nueva categoría' })
+  @ApiResponse({
+    status: 201,
+    description: 'Categoría creada exitosamente',
+    type: Categoria,
+  })
   crearCategoria(@Body() createCategoriaDto: CreateCategoriaDto) {
     return this.categoriasService.crearCategoria(createCategoriaDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Obtener todas las categorías' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de todas las categorías',
+    type: Categoria,
+    isArray: true,
+  })
   obtenerTodasLasCategorias() {
     return this.categoriasService.obtenerTodasLasCategorias();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Obtener una categoría por ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Categoría encontrada exitosamente',
+    type: Categoria,
+  })
+  @ApiResponse({ status: 404, description: 'Categoría no encontrada' })
   obtenerUnaCategoria(@Param('id') id: string) {
     return this.categoriasService.obtenerUnaCategoria(+id);
   }
 
   @Patch(':id')
-  actualizarCategoria(@Param('id') id: string, @Body() updateCategoriaDto: UpdateCategoriaDto) {
+  @ApiOperation({ summary: 'Actualizar una categoría por ID' })
+  @ApiResponse({
+    status: 200,
+    description: 'Categoría actualizada exitosamente',
+    type: Categoria,
+  })
+  @ApiResponse({ status: 404, description: 'Categoría no encontrada' })
+  actualizarCategoria(
+    @Param('id') id: string,
+    @Body() updateCategoriaDto: UpdateCategoriaDto,
+  ) {
     return this.categoriasService.actualizarCategoria(+id, updateCategoriaDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar una categoría por ID' })
+  @ApiResponse({ status: 204, description: 'Categoría eliminada exitosamente' })
+  @ApiResponse({ status: 404, description: 'Categoría no encontrada' })
   eliminarCategoria(@Param('id') id: string) {
     return this.categoriasService.eliminarCategoria(+id);
   }
--- src/categorias/categorias.service.ts
+++ src/categorias/categorias.service.ts
@@ -1,26 +1,113 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+  Logger,
+  NotFoundException,
+} from '@nestjs/common';
 import { CreateCategoriaDto } from './dto/create-categoria.dto';
 import { UpdateCategoriaDto } from './dto/update-categoria.dto';
+import { Categoria } from './entities/categoria.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CategoriasService {
-  crearCategoria(createCategoriaDto: CreateCategoriaDto) {
-    return 'This action adds a new categoria';
+  private readonly logger = new Logger('CategoriasService');
+
+  constructor(
+    @InjectRepository(Categoria)
+    private readonly categoriaRepository: Repository<Categoria>,
+  ) {}
+
+  async crearCategoria(createCategoriaDto: CreateCategoriaDto) {
+    const { nombre } = createCategoriaDto;
+
+    const existingCategoria = await this.categoriaRepository.findOne({
+      where: { nombre },
+    });
+
+    if (existingCategoria) {
+      throw new ConflictException(
+        'Ya existe una categoría con el mismo nombre.',
+      );
+    }
+
+    try {
+      const categoria = this.categoriaRepository.create(createCategoriaDto);
+      await this.categoriaRepository.save(categoria);
+      return categoria;
+    } catch (error) {
+      this.ManejadorDeErrores(error);
+    }
   }
 
   obtenerTodasLasCategorias() {
-    return `This action returns all categorias`;
+    return this.categoriaRepository.find();
   }
 
-  obtenerUnaCategoria(id: number) {
-    return `This action returns a #${id} categoria`;
+  async obtenerUnaCategoria(term: number) {
+    let categoria: Categoria;
+
+    // Verificar si el término es un número válido
+    if (isNaN(term)) {
+      throw new NotFoundException('El término debe ser un número válido');
+    }
+
+    // Buscar por ID
+    categoria = await this.categoriaRepository.findOneBy({ id: term });
+
+    // Si no se encuentra por ID, buscar por nombre
+    if (!categoria) {
+      const queryBuilder =
+        this.categoriaRepository.createQueryBuilder('categoria');
+      categoria = await queryBuilder
+        .where('LOWER(categoria.name) = :term', {
+          term: String(term).toLowerCase(),
+        })
+        .getOne();
+    }
+
+    // Si no se encuentra la categoría, lanzar una excepción
+    if (!categoria) {
+      throw new NotFoundException(
+        `Categoría con el término '${term}' no encontrada`,
+      );
+    }
+
+    return categoria;
   }
 
-  actualizarCategoria(id: number, updateCategoriaDto: UpdateCategoriaDto) {
-    return `This action updates a #${id} categoria`;
+  async actualizarCategoria(
+    id: number,
+    updateCategoriaDto: UpdateCategoriaDto,
+  ) {
+    const categoria = await this.categoriaRepository.preload({
+      id: id,
+      ...updateCategoriaDto,
+    });
+
+    if (!categoria)
+      throw new NotFoundException(`Marca con ID ${id} no encontrada`);
+
+    try {
+      await this.categoriaRepository.save(categoria);
+      return categoria;
+    } catch (error) {
+      this.ManejadorDeErrores(error);
+    }
   }
 
-  eliminarCategoria(id: number) {
-    return `This action removes a #${id} categoria`;
+  async eliminarCategoria(id: number) {
+    const categoria = await this.obtenerUnaCategoria(id);
+    await this.categoriaRepository.remove(categoria);
+  }
+
+  private ManejadorDeErrores(error: any) {
+    this.logger.error(error);
+
+    throw new InternalServerErrorException(
+      'Ocurrió un error inesperado. Por favor, verifica los registros del servidor.',
+    );
   }
 }
